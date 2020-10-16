211dea40e706f0e5751628d7697fdd0354615b1b
Victor <victorassisrd@gmail.com>
2020-10-15 21:54:43 -0300

    Exercicio Interface IRepository
    Exercicio Interface IRepository

---
+++ ProjBiblio/ProjBiblio.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ProjBiblio.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Get();
+        T GetById(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+
+}
---
+++ ProjBiblio/ProjBiblio.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using ProjBiblio.Domain.Entities;
+
+namespace ProjBiblio.Domain.Interfaces
+{
+    public interface IUsuarioRepository  : IRepository<Usuario>
+    {
+    }
+}
